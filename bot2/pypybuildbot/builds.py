dbd53e485f9c380752d4c2aec06f99ac9c749cc0
pedronis <pedronis@localhost>
2008-11-01 15:23:23 +0000

    good to go

--- bot2/pypybuildbot/builds.py
+++ bot2/pypybuildbot/builds.py
@@ -98,7 +98,7 @@ class PyPyOwnTestFactory(factory.BuildFactory):
                      "--logfile=testrun.log",
                      "--config=pypy/testrunner_cfg.py",
                      "--config=~/machine_cfg.py",
-                     "--root=pypy", "--dry-run", "--timeout=3600"],
+                     "--root=pypy", "--timeout=3600"],
             logfiles={'pytestLog': 'testrun.log'},
             timeout = 4000,
             env={"PYTHONPATH": ['.']}))
