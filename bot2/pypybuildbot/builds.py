1970d5295057bd7f37e7a0aa2d1dc8259aeec4a6
fijal <fijal@localhost>
2008-11-08 11:45:03 +0000

    we need to call this

--- bot2/pypybuildbot/builds.py
+++ bot2/pypybuildbot/builds.py
@@ -143,13 +143,13 @@ class PyPyTranslatedAppLevelTestFactory(factory.BuildFactory):
 
 class PyPyTranslatedScratchboxTestFactory(factory.BuildFactory):
     def __init__(self, *a, **kw):
+        factory.BuildFactory.__init__(self, *a, **kw)
         platform = kw.pop('platform', 'linux')
         setup_steps(platform, self)
         return
         USERNAME = os.environ['HOME'].split(os.sep)[-1]
         SCRATCHBOX_WORKDIR = ("/scratchbox/users/%s/home/%s/build" %
                               (USERNAME, USERNAME))
-        factory.BuildFactory.__init__(self, *a, **kw)
         setup_steps(platform, self, workdir=SCRATCHBOX_WORKDIR)
         workdir = os.path.join(SCRATCHBOX_WORKDIR, 'pypy', 'translator', 'goal')
         self.addStep(Translate(["--platform", "maemo", "-Omem"], [],
