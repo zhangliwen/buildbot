6067aa8fc849cbd3a37d76dd066b7243a6076344
pedronis <pedronis@localhost>
2009-02-21 17:36:07 +0000

    schedule the app-level stackless tests too

--- bot2/pypybuildbot/master.py
+++ bot2/pypybuildbot/master.py
@@ -49,7 +49,8 @@ BuildmasterConfig = {
 
     'change_source': [],
     'schedulers': [
-    	Nightly("nightly", [LINUX32, CPYLINUX32, APPLVLLINUX32, CPYWIN32],
+    	Nightly("nightly", [LINUX32, CPYLINUX32, APPLVLLINUX32, CPYWIN32,
+                            STACKLESSAPPLVLLINUX32],
                 hour=4, minute=45),
     ],   
     'status': [status],
