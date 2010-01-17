ea51035e1b931dbdd7bbac80168d2fc60ffe5bd4
fijal <fijal@localhost>
2010-01-17 19:50:03 +0000

    Add a small summary that displays small graphs

--- bot2/jsplot/js/plot.js
+++ bot2/jsplot/js/plot.js
@@ -9,18 +9,9 @@ if (window.location.toString().indexOf('file:///') == -1) {
     JSON_DIR_LIST = JSON_DIR_URL + "dir";
 }
 
-function plot_main(benchname, benchresults, cpython_results) {
-    $("#placeholder").append("<p class='caption'>" + benchname + "</p>");
-    $("#placeholder").append("<div class='plot'></div>");
-    var plotinput = [{
-        label: 'pypy-c-jit',
-        data : benchresults,
-    },
-    {
-        label: 'cpython',
-        data : cpython_results
-    }];
-    $.plot($("#placeholder").children(":last"), plotinput, {
+function common_attrs()
+{
+    return {
         'series': {
             'points': {'show': true},
             'lines' : {'show': true},
@@ -33,8 +24,41 @@ function plot_main(benchname, benchresults, cpython_results) {
             'min': 0,
         },
         'legend' : {
-            'position' : 'sw'
+            'position': 'nw',
         }
-    });
+    }
 }
 
+function get_plot_input(benchresults, cpython_results)
+{
+    return [{
+        label: 'pypy-c-jit',
+        data : benchresults,
+    },
+    {
+        label: 'cpython',
+        data : cpython_results
+    }];
+}
+
+var MAX_PER_LINE = 4;
+
+function plot_miniature(benchname, benchresults, cpython_results)
+{
+    if ($("#placeholder").find("td").length % MAX_PER_LINE == 0) {
+        $("#placeholder").append("<tr></tr>");
+    }
+    $("#placeholder").find("tr:last").append("<td><p class='smallcaption'>" + benchname + "</p><div class='miniplot'></div></td>");
+    var elem = $("#placeholder").find("div:last");
+    var attrs = common_attrs();
+    attrs.xaxis.ticks = 0;
+    attrs.yaxis.ticks = 0;
+    $.plot(elem, [benchresults, cpython_results], attrs);
+}
+
+function plot_main(benchname, benchresults, cpython_results) {
+    $("#placeholder").append("<p class='caption'>" + benchname + "</p>");
+    $("#placeholder").append("<div class='plot'></div>");
+    var plotinput = get_plot_input(benchresults, cpython_results)
+    $.plot($("#placeholder").children(":last"), plotinput, common_attrs());
+}
