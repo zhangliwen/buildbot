7151ecabe34ed451254f0c2f97bba8db57741338
xoraxax <xoraxax@localhost>
2010-03-13 18:04:52 +0000

    Move Psyco step to the beginning.

--- bot2/pypybuildbot/builds.py
+++ bot2/pypybuildbot/builds.py
@@ -127,6 +127,18 @@ class JITBenchmark(factory.BuildFactory):
             command=['svn', 'co', 'http://codespeak.net/svn/pypy/benchmarks',
                      'benchmarks'],
             workdir='.'))
+
+        self.addStep(ShellCmd(
+            description="run benchmarks on top of python with psyco",
+            command=["python", "runner.py", '--output-filename', 'result.json',
+                    '--pypy-c', 'psyco/python_with_psyco.sh',
+                     '--revision', WithProperties('%(got_revision)s'),
+                     '--upload', '--force-host', 'bigdog',
+                     '--branch', WithProperties('%(branch)s'),
+                     ],
+            workdir='./benchmarks',
+            haltOnFailure=True))
+
         self.addStep(Translate(['-Ojit'], []))
         self.addStep(ShellCmd(
             description="run more benchmarks on top of pypy-c-jit",
@@ -157,16 +169,7 @@ class JITBenchmark(factory.BuildFactory):
                                          masterdest=WithProperties(resfile),
                                          workdir="."))
 
-        self.addStep(ShellCmd(
-            description="run benchmarks on top of python with psyco",
-            command=["python", "runner.py", '--output-filename', 'result.json',
-                    '--pypy-c', 'psyco/python_with_psyco.sh',
-                     '--revision', WithProperties('%(got_revision)s'),
-                     '--upload', '--force-host', 'bigdog',
-                     '--branch', WithProperties('%(branch)s'),
-                     ],
-            workdir='./benchmarks',
-            haltOnFailure=True))
+
 
         self.addStep(ShellCmd(
             description="run benchmarks 1",
