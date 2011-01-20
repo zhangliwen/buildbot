da756db6a00e4265306a0b7e9de172f8fda85a80
? <victorgarcianet@gmail.com>
2011-01-20 14:32:16 -0200

    Remove bogus test data.

--- bitbucket_hook/hook.py
+++ bitbucket_hook/hook.py
@@ -122,7 +122,7 @@ class BitbucketHookHandler(object):
                 print "IRC messages:"
             self.send_irc_message(irc_msg, test)
             if test:
-                print "\n"
+                print
 
     def handle_diff_email(self, test=False):
         import operator
@@ -168,20 +168,14 @@ if __name__ == '__main__':
     import hook as hookfile
     repopath = os.path.dirname(os.path.dirname(hookfile.__file__))
     print 'Repository path:', repopath
-    test_payload = {u'commits': [
-##        {u'author': u'antocuni',
-##                             u'branch': u'default',
-##                             u'files': [{u'file': u'pdbdemo2.py', u'type': u'modified'},
-##                                        {u'file': u'README2', u'type': u'added'},
-##                                        {u'file': u'xx.png', u'type': u'added'}],
-##                             u'message': u'test for git diff',
-##                             u'node': u'81d52c8e34ba',
-##                             u'parents': [u'3d60a8c359a3'],
-##                             u'raw_node': u'81d52c8e34ba44bd34780906064eee12b81cb82b',
-##                             u'revision': 21,
-##                             u'size': 2439,
-##                             u'timestamp': u'2010-12-17 12:00:24'},
+    test_payload = {u'repository': {u'absolute_url': '',
+                                    u'name': u'test',
+                                    u'owner': u'antocuni',
+                                    u'slug': u'test',
+                                    u'website': u''},
+                    u'user': u'antocuni'}
 
+    test_payload[u'commits'] = [
                             {u'author': u'antocuni',
                             u'branch': u'default',
                             u'files': [{u'file': u'bitbucket_hook/hook.py', u'type': u'modified'},
@@ -194,30 +188,12 @@ if __name__ == '__main__':
                             u'raw_node': u'9c7bc068df8850f4102c610d2bee3cdef67b30e6',
                             u'revision': 391,
                             u'size': 753,
-                            u'timestamp': u'2010-12-19 14:45:44'},
-##
-##                            {u'author': u'antocuni',
-##                             u'branch': u'default',
-##                             u'files': [{u'file': u'pdbdemo2.py', u'type': u'modified'},
-##                                        {u'file': u'README2', u'type': u'modified'}],
-##                             u'message': u'Chapter 11. Customizing the output of Mercurial\n\nTable of Contents\n\nLoad all comments (slow)\n\nUsing precanned output styles\n\n    Setting a default style\n\nCommands that support styles and templates\nThe basics of templating\nCommon template keywords\nEscape sequences\nFiltering keywords to change their results',
-##                             u'node': u'47b731b0f331',
-##                             u'parents': [u'e26b0d04f68e'],
-##                             u'raw_node': u'47b731b0f3312ab3302f4b45f1ec28292f6a2bcc',
-##                             u'revision': 24,
-##                             u'size': 1141,
-##                             u'timestamp': u'2010-12-17 14:28:12'}],
-],
-               u'repository': {u'absolute_url': '',
-                               u'name': u'test',
-                               u'owner': u'antocuni',
-                               u'slug': u'test',
-                               u'website': u''},
-               u'user': u'antocuni'}
+                            u'timestamp': u'2010-12-19 14:45:44'}]
+
 ##    # To regenerate:
 ##    from urllib2 import urlopen
 ##    url = ("https://api.bitbucket.org/1.0/repositories/pypy/buildbot/"
-##           "changesets/?start=3d60a8c359a3&limit=4")
+##           "changesets/%s/")
 ##    req = urlopen(url)
 ##    test_payload = req.read()
 ##    req.close()
