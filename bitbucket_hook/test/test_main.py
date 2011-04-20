a0e445bb7cea437355ce37863d8a73f14c5ca030
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2011-04-20 10:19:22 +0200

    add some basic tests for the data pass-trough of the post handler

---
+++ bitbucket_hook/test/test_main.py
@@ -0,0 +1,32 @@
+from bitbucket_hook.main import app
+from bitbucket_hook import hook
+
+def test_get():
+    client = app.test_client()
+    response = client.get('/')
+
+
+def test_post(monkeypatch):
+    client = app.test_client()
+    def handle(payload, test):
+        assert payload=={}
+        assert test==app.config['TESTING']
+    monkeypatch.setattr(hook, 'handle', handle)
+
+    app.config['TESTING'] = True
+    response = client.post('/', data={'payload':"{}"})
+
+    app.config['TESTING'] = False
+    response = client.post('/', data={'payload':"{}"})
+
+    assert response.status_code == 200
+
+def test_post_error(monkeypatch):
+    client = app.test_client()
+    def handle(payload, test):
+        raise Exception('omg')
+    monkeypatch.setattr(hook, 'handle', handle)
+    response = client.post('/', data={'payload':"{}"})
+    assert response.status_code == 500
+
+
