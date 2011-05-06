ad856916ac4a265cacb3a5b66a47846c78e73770
Antonio Cuni <anto.cuni@gmail.com>
2011-05-06 12:24:10 +0200

    s/codespeak.net/wyvern/, and add xerxes as a buildslave

--- bot2/pypybuildbot/util.py
+++ bot2/pypybuildbot/util.py
@@ -1,7 +1,7 @@
 import socket
 
 def we_are_debugging():
-    return socket.gethostname() not in ("code0.codespeak.net",)
+    return socket.gethostname() not in ("wyvern",)
 
 def load(name):
     mod = __import__(name, {}, {}, ['__all__'])
