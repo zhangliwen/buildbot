17b7997b2b2d855da329eb332f412814ece028a5
Antonio Cuni <anto.cuni@gmail.com>
2011-05-09 17:50:02 +0200

    don't bind to a specific hostname

--- bitbucket_hook/run.py
+++ bitbucket_hook/run.py
@@ -13,7 +13,8 @@ main = py.path.local(__file__).dirpath().join('main.py').pyimport()
 
 
 if __name__ == '__main__':
-    HOST_NAME = 'wyvern.cs.uni-duesseldorf.de'
+    #HOST_NAME = 'wyvern.cs.uni-duesseldorf.de'
+    HOST_NAME = ''
     PORT_NUMBER = 9237
     main.app.run(
         host = HOST_NAME if 'deploy' in sys.argv else 'localhost',
