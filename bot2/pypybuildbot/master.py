96a90d96e5f0e60ab51bb40fd8cc1ba64ff3bb9e
David Schneider <david.schneider@picle.org>
2012-07-13 11:05:10 +0200

    make the channel depend on the debugging flag

--- bot2/pypybuildbot/master.py
+++ bot2/pypybuildbot/master.py
@@ -6,6 +6,7 @@ from buildbot.process.builder import Builder
 #from buildbot import manhole
 from pypybuildbot.pypylist import PyPyList
 from pypybuildbot.ircbot import IRC # side effects
+from pypybuildbot.util import we_are_debugging
 
 # Forbid "force build" with empty user name
 from buildbot.status.web.builder import StatusResourceBuilder
@@ -18,7 +19,7 @@ if _previous_force.__name__ == 'force':
     StatusResourceBuilder.force = my_force
 # Done
 
-if getpass.getuser() == 'antocuni':
+if we_are_debugging():
     channel = '#buildbot-test'
 else:
     channel = '#pypy'
