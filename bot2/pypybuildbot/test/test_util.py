72c076ca2244d3eb5977138d9f57d9f4360f63dd
mattip <matti.picus@gmail.com>
2015-10-17 23:31:05 +0300

    when using PyPyUpload remove old files and empty directories (implement, test)

--- bot2/pypybuildbot/test/test_util.py
+++ bot2/pypybuildbot/test/test_util.py
@@ -8,3 +8,17 @@ def test_symlink_force(tmpdir):
     assert latest.readlink() == str(one)
     util.symlink_force(str(two), str(latest))
     assert latest.readlink() == str(two)
+
+def test_clean_old_files(tmpdir):
+    one = tmpdir.join('one/tmp.txt').ensure(file=True)
+    two = tmpdir.join('one/two/tmp.txt').ensure(file=True)
+    two.setmtime(two.mtime() - 90*24*60*60)
+    util.clean_old_files(one.dirname, 40)
+    assert one.exists()
+    assert not two.exists()
+    assert two.dirpath().exists()
+    # empty directory only removed on second run
+    util.clean_old_files(one.dirname, 40)
+    assert not two.dirpath().exists()
+    
+    
--- bot2/pypybuildbot/util.py
+++ bot2/pypybuildbot/util.py
@@ -1,5 +1,6 @@
 import os
 import socket
+from datetime import datetime, timedelta
 
 def we_are_debugging():
     return socket.gethostname() != 'cobra'
@@ -17,3 +18,20 @@ def symlink_force(src, dst):
     if os.path.lexists(dst):
         os.remove(dst)
     os.symlink(src, dst)
+
+def clean_old_files(dest, oldest_in_days):
+    '''
+    remove files from dest older than 'older_in_days' days
+    and remove empty directories as well
+    '''
+
+    old_hours = 24 * oldest_in_days
+    for dirpath, dirnames, filenames in os.walk(dest):
+        for fname in filenames:
+            curpath = os.path.join(dirpath, fname)
+            mtime = datetime.fromtimestamp(os.path.getmtime(curpath))
+            if datetime.now() - mtime > timedelta(hours=old_hours):
+                os.remove(curpath)
+        if len(filenames) == 0:
+            os.rmdir(dirpath)
+ 
