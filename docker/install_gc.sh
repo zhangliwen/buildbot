cf42d14cf627abb4c1a2dc7bff8f1ae13b35d031
Matti Picus <matti.picus@gmail.com>
2019-12-02 14:54:05 +0200

    add local build of gc since centos gc-devel is too old

---
+++ docker/install_gc.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -xe
+
+mkdir boehm
+pushd boehm
+wget -q https://github.com/ivmai/libatomic_ops/releases/download/v7.6.10/libatomic_ops-7.6.10.tar.gz
+mkdir libatomic_ops
+tar -C libatomic_ops --strip-components=1 -xf libatomic_ops-7.6.10.tar.gz
+wget -q https://github.com/ivmai/bdwgc/releases/download/v8.0.4/gc-8.0.4.tar.gz
+mkdir bdwgc
+tar -C bdwgc --strip-components=1 -xf gc-8.0.4.tar.gz
+ln -s  $PWD/libatomic_ops $PWD/bdwgc/libatomic_ops
+cd bdwgc
+autoreconf -vif
+automake --add-missing
+./configure
+make
+make install
+popd
+rm -rf  boehm
